a0ccc5795a709dabdc18bab8813190bcd1a9ab33
Frederick Johnson <fritzj96@gmail.com>
2019-11-13 15:58:13 -0800

    added AccountCheckFundsAvailable view

--- account/views.py
+++ account/views.py
@@ -1,7 +1,10 @@
 from .serializers import AccountSerializer
 from .models import Account
+from rest_framework.views import APIView
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-
+from django.http import Http404
+from rest_framework.response import Response
+from django.utils.timezone import datetime
 # Create your views here.
 # class AccountViewSet(ModelViewSet):
 #   """
@@ -21,4 +24,15 @@ class AccountDetail(RetrieveUpdateDestroyAPIView):
   queryset = Account.objects.all()
   lookup_field = 'user_id'
   serializer_class = AccountSerializer
-  
+
+class AccountCheckFundsAvailable(APIView):
+  """
+  Checks if the matching account in 
+  """
+  def get(self, request, user_id):
+    account = Account.objects.get(user_id=user_id)
+    amountParam = request.GET.get('amount')
+    currencyParam = request.GET.get('currency')
+    now=str(datetime.now())
+    if account.balance_currency == currencyParam and float(account.balance_amount) > float(amountParam):
+      return Response({ "answer": "yes", "date": now }, content_type='application/json')
