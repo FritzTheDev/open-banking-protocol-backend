980d9eb099fae319a522e808c71244a12f49453d
Frederick Johnson <fritzj96@gmail.com>
2019-11-13 14:10:15 -0800

    added a ReadOnly Viewset for the Account Model that provides list & detail actions

--- account/views.py
+++ account/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ReadOnlyModelViewSet
+from .serializers import AccountSerializer
+from .models import Account
 
 # Create your views here.
+class AccountViewSet(ReadOnlyModelViewSet):
+  """
+  ### AccountViewSet
+  This viewset automatically gives me "list" & 
+  "detail" api views, or "actions", if you will.
+  """
+  queryset = Account.objects.all()
+  serializer_class = AccountSerializer
