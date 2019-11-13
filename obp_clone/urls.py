840d8e1e33f083f59f066c0a8ebe1eff15e2b481
Frederick Johnson <fritzj96@gmail.com>
2019-11-13 14:58:04 -0800

    BF: fixed URL pattern bug that  showed up on first run

--- obp_clone/urls.py
+++ obp_clone/urls.py
@@ -18,7 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/api/version/<str:api_version>/banks/<str:bank>/', include([
-        'account.urls'
-    ]))
+    path('/api/version/<str:api_version>/banks/<str:bank>/', include('account.urls', namespace="account"))
 ]
