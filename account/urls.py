d8c81ccfb9a076768c5515dc4794ad900c5cce1c
Frederick Johnson <fritzj96@gmail.com>
2019-11-13 15:23:36 -0800

    fixed some dumb routing by switching back to Generic Views

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from account.views import AccountViewSet
-
-router = DefaultRouter()
-router.register('accounts', AccountViewSet)
+from account.views import AccountDetail, AccountList
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', AccountList),
+    path('<str: user_id>', AccountDetail),
 ]
--- account/views.py
+++ account/views.py
@@ -1,13 +1,24 @@
-from rest_framework.viewsets import ModelViewSet
 from .serializers import AccountSerializer
 from .models import Account
+from rest_framework.generics import ListCreateAPIView, RetrieveAPIView
 
 # Create your views here.
-class AccountViewSet(ModelViewSet):
-  """
-  This view gives me retrieve, list, create, update, and delete actions. 
+# class AccountViewSet(ModelViewSet):
+#   """
+#   This view gives me retrieve, list, create, update, and delete actions. 
   
-  Use PUT to create new accounts at a specific ID, not POST to create new ones from scratch
-  """
+#   Use PUT to create new accounts at a specific ID, not POST to create new ones from scratch
+#   """
+#   queryset = Account.objects.all()
+#   serializer_class = AccountSerializer
+
+
+class AccountList(ListCreateAPIView):
   queryset = Account.objects.all()
   serializer_class = AccountSerializer
+
+class AccountDetail(RetrieveAPIView):
+  queryset = Account.objects.all()
+  lookup_field = 'user_id'
+  serializer_class = AccountSerializer
+  
