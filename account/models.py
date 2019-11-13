1b851ab450ffea71948e403d9970657dad524cd2
Frederick Johnson <fritzj96@gmail.com>
2019-11-13 14:37:17 -0800

    DEV: Account model added

--- account/models.py
+++ account/models.py
@@ -1,3 +1,28 @@
-from django.db import models
+from django.db.models import Model, CharField,
 
 # Create your models here.
+class Account(Model):
+  """
+  ### Account Model
+  Contains following data
+  - user_id: str (Max 255 char && unique)
+  - label: str (Max 255 char)
+  - type: str (see docs for Product.product_code)
+  - balance: dict (keys: "currency": str && "amount": str (amount MUST BE 0))
+  - branch_id: str
+  - account_routing: dict (keys: "scheme": str && "address": str)
+
+  ### NOT IMPLEMENTED ###
+  - account_attributes: list of AccountAttributes. AccountAttributes are
+  automatically assigned based on the product_code specified in type member
+  """
+
+
+  user_id = CharField(max_length=255)
+  label = CharField(max_length=255)
+  type = CharField(max_length=10)
+  balance_currency = CharField(max_length=3)
+  balance_amount = CharField(default=0)
+  branch_id = CharField(max_length=20)
+  account_routing_scheme = CharField(max_length=30)
+  account_routing_address = CharField(max_length=30)
